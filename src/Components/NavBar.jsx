1c8f00da34962e77e79b136c018011d1c4c00395
Your Name <dheerajks412@gmail.com>
2022-10-14 12:39:45 +0530

    Initial commit

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <>
+      <section>
+        <nav className="navbar">
+          <div className="container-fluid">
+            <div className="container">
+              <h1>
+                TV-Maze Search App
+              </h1>
+            </div>
+          </div>
+        </nav>
+      </section>
+    </>
+  );
+}
+
+export default NavBar;
